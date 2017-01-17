d66cb2db4409f85fae29863be73e3772b60085f0
VanHakobyan <van19962013@mail.ru>
2017-01-17 21:29:49 +0400

    edited

--- CryptoServiceClassLibrary/CryptoServiceClassLibrary/rngCrypt.cs
+++ CryptoServiceClassLibrary/CryptoServiceClassLibrary/rngCrypt.cs
@@ -4,83 +4,84 @@ using System.Text;
 
 namespace CryptoServiceClassLibrary
 {
-    public class rngCrypt
+    public class Password
     {
-        public enum optionPass
+        private string getPassword;
+
+        private string lower = "abcdefghijklmnopqrstuvwxyz";
+        private string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private string symbols = " !#$%&'()*+,-./:;<=>?@[\"]^_`\\{|}~";
+        private string numbers = "0123456789";
+
+        public Password NewPassword(optionPass option = optionPass.pasDigitUpperSmall, int length = 32)
         {
-            pasSize,
-            pasUpperOnly,
-            pasSmallOnly,
-            pasDigitOnly,
-            pasDigitUpperSmall
-
-        }
-        public class Password
-        {
-            private string getPassword;
-
-            private string lower = "abcdefghijklmnopqrstuvwxyz";
-            private string upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            private string symbols = " !#$%&'()*+,-./:;<=>?@[\"]^_`\\{|}~";
-            private string numbers = "0123456789";
-
-            public Password NewPassword(optionPass option = optionPass.pasDigitUpperSmall, int length = 32)
+            using (var rng = new RNGCryptoServiceProvider())
             {
-                using (var rng = new RNGCryptoServiceProvider())
+                var data = new byte[4];
+                rng.GetBytes(data);
+                var seed = BitConverter.ToInt32(data, 0);
+                var rnd = new Random(seed);
+
+                var passChars = new char[length];
+
+                var sb = new StringBuilder();
+                for (var i = 0; i < passChars.Length; i++)
                 {
-                    var data = new byte[4];
-                    rng.GetBytes(data);
-                    var seed = BitConverter.ToInt32(data, 0);
-                    var rnd = new Random(seed);
 
-                    var passChars = new char[length];
-
-                    var sb = new StringBuilder();
-                    for (var i = 0; i < passChars.Length; i++)
+                    if (option == optionPass.pasSize)
+                        sb.Append(lower[rnd.Next(0, 62)]);
+                    if (option == optionPass.pasUpperOnly)
+                        sb.Append(upper[rnd.Next(0, 36)]);
+                    if (option == optionPass.pasSmallOnly)
+                        sb.Append(symbols[rnd.Next(0, 36)]);
+                    if (option == optionPass.pasDigitOnly)
+                        sb.Append(numbers[rnd.Next(0, 52)]);
+                    if (option == optionPass.pasDigitUpperSmall)
                     {
-
-                        if (option == optionPass.pasSize) sb.Append(lower[rnd.Next(0, 62)]);
-                        if (option == optionPass.pasUpperOnly) sb.Append(upper[rnd.Next(0, 36)]);
-                        if (option == optionPass.pasSmallOnly) sb.Append(symbols[rnd.Next(0, 36)]);
-                        if (option == optionPass.pasDigitOnly) sb.Append(numbers[rnd.Next(0, 52)]);
-                        if (option == optionPass.pasDigitUpperSmall)
-                        {
-                            if (sb.Length == rnd.Next(sb.Length, length + 1)) sb.Append(symbols[rnd.Next(0, 33)]);
-                            else if (sb.Length == rnd.Next(sb.Length, length + 1)) sb.Append(numbers[rnd.Next(0, 10)]);
-                            else sb.Append(lower[rnd.Next(0, 52)]);
-                        }
+                        if (sb.Length == rnd.Next(sb.Length, length + 1)) sb.Append(symbols[rnd.Next(0, 33)]);
+                        
+                        else sb.Append(lower[rnd.Next(0, 52)]);
                     }
-
-                    var pass = new Password(sb.ToString());
-
-                    return pass;
                 }
-            }
 
-            public Password()
-            {
-                getPassword = NewPassword().ToString();
+                var pass = new Password(sb.ToString());
+
+                return pass;
             }
-            public Password(int length)
-            {
-                getPassword = NewPassword(optionPass.pasDigitUpperSmall, length).ToString();
-            }
-            public Password(string str)
-            {
-                getPassword = str;
-            }
-            public Password(optionPass passOpt)
-            {
-                getPassword = NewPassword(passOpt).ToString();
-            }
-            public Password(optionPass passOpt, int length)
-            {
-                getPassword = NewPassword(passOpt, length).ToString();
-            }
-            public new string ToString()
-            {
-                return getPassword;
-            }
+        }
+
+        public Password()
+        {
+            getPassword = NewPassword().ToString();
+        }
+        public Password(int length)
+        {
+            getPassword = NewPassword(optionPass.pasUpperOnly, length).ToString();
+        }
+        public Password(string str)
+        {
+            getPassword = str;
+        }
+        public Password(optionPass passOpt)
+        {
+            getPassword = NewPassword(passOpt).ToString();
+        }
+        public Password(optionPass passOpt, int length)
+        {
+            getPassword = NewPassword(passOpt, length).ToString();
+        }
+        public new string ToString()
+        {
+            return getPassword;
         }
     }
+}
+public enum optionPass
+{
+    pasSize,
+    pasUpperOnly,
+    pasSmallOnly,
+    pasDigitOnly,
+    pasDigitUpperSmall
+
 }
---
+++ CryptoServiceClassLibrary/Testing/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CryptoServiceClassLibrary;
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pas = new Password(5);
+            Console.WriteLine(pas);
+        }
+    }
+}
